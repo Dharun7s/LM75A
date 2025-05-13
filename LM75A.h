d70dbb82b2ca0c1a5803e5ffd8f79ee9d22c5c83
Dharun7s <149937134+Dharun7s@users.noreply.github.com>
2025-05-13 21:48:45 +0530

    LM75A.h

---
+++ LM75A.h
@@ -0,0 +1,19 @@
+#ifndef LM75A_H
+#define LM75A_H
+
+#include "hardware/i2c.h"
+#include "pico/stdlib.h"
+
+class LM75A {
+public:
+    LM75A(i2c_inst_t* i2c_port, uint8_t address);
+    void begin();
+    float readTemperature();
+
+private:
+    i2c_inst_t* i2c;
+    uint8_t addr;
+    uint16_t read16(uint8_t reg);
+};
+
+#endif
